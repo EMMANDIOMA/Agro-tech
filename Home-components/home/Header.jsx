4e4523506ba043efd918aa0e178a484c2d792bbb
Creator <chuksisaac62@gmail.com>
2026-07-16 17:00:14 +0100

    Add files via upload

---
+++ Home-components/home/Header.jsx
@@ -0,0 +1,12 @@
+import 'react';
+
+const Header = () => {
+  return (
+    <div className="px-6 py-6 space-y-2">
+      <p className="font-sans text-[#999999]">Good Morning,</p>
+      <h2 className="text-4xl text-[#2C2C2C] font-bold">Emmanuel 👋</h2>
+    </div>
+  );
+};
+
+export default Header;
